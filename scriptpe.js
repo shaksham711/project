9e143358bb52358ee8a84275ce128aabd20a80cd
shaksham711 <84431830+shaksham711@users.noreply.github.com>
2022-04-17 10:55:24 +0530

    Add files via upload

---
+++ scriptpe.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".typing", {
+    strings: ["Men", "Women", "Kids", "Adults"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["Men", "Women", ""],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
